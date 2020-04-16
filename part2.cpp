0248f2c1872f5c8f9065ea92dcd1e2e818204052
Griffin Quarles <gquarles@protonmail.com>
2020-04-16 13:21:20 -0400

    code fin

--- part2.cpp
+++ part2.cpp
@@ -1,5 +1,6 @@
-// asm23.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//CSC 362 Homework 6
+//Author: Griffin Quarles
+
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -13,44 +14,44 @@ int main() {
 	int temp;
 	int location;
 
-	for (int i = 1; i < n; i++) {
-		__asm {
-			mov ecx, i //Load iterator into ecx
-			mov eax, a[ecx * 4] //Load a[i]
-			mov temp, eax       //temp = a[i]
-			dec ecx //dec location
-			mov location, ecx   //location = i - 1
-			inc ecx //Inc ecx after saving location
+	__asm {
+		mov ecx, 1 //i = 1
+		top: nop //Top of for loop
 
+		mov eax, a[ecx * 4] //Load a[i]
+		mov temp, eax       //temp = a[i]
+		dec ecx //dec location
+		mov location, ecx   //location = i - 1
+		inc ecx //Inc ecx after saving location
 
-			whiletop: nop //Top of while loop
-			cmp location, 0 //location >= 0
-			jl whileout //Leave while loop
+		whiletop: nop //Top of while loop
+		cmp location, 0 //location >= 0
+		jl whileout //Leave while loop
 
-			mov eax, location //Load location into eax
-			mov ebx, a[eax*4] //Load a[location] into ebx
-			cmp ebx, temp //a[location > temp]
-			jl whileout //exit loop
+		mov eax, location //Load location into eax
+		mov ebx, a[eax*4] //Load a[location] into ebx
+		cmp ebx, temp //a[location > temp]
+		jl whileout //exit loop
 
-			//mov eax, location //Load in location
-			mov ebx, a[eax * 4] //Load in the var from array into ebx
-			inc eax //incrment location
-			mov a[eax * 4], ebx //Save var in array  //a[location + 1] = a[location];
-			dec eax //Decrement by 2               //location--;
-			dec eax //eax is location
-			mov location, eax //Save location
-			jmp whiletop //Go back to while loop (location >= 0 && a[location] > temp)
+		//mov eax, location //Load in location
+		mov ebx, a[eax * 4] //Load in the var from array into ebx
+		inc eax //incrment location
+		mov a[eax * 4], ebx //Save var in array  //a[location + 1] = a[location];
+		dec eax //Decrement by 2               //location--;
+		dec eax //eax is location
+		mov location, eax //Save location
+		jmp whiletop //Go back to while loop (location >= 0 && a[location] > temp)
 
-			whileout:nop
-			mov eax, location //Move location into eax
-			inc eax //increment eax to go further in the array
-			mov ebx, temp //load temp into ebx
-			mov a[eax * 4], ebx //save a[location + 1] = temp;
+		whileout:nop
+		mov eax, location //Move location into eax
+		inc eax //increment eax to go further in the array
+		mov ebx, temp //load temp into ebx
+		mov a[eax * 4], ebx //save a[location + 1] = temp;
 
-		}
+		inc ecx //i++
+		cmp ecx, n //if i < n 
+		jl top //Jump to top of loop
 	}
-
-
 
 
 	for (int z = 1; z < n; z++) {
