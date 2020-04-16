0c2df51fc35177a0528c23f85a4ee9885e225e77
Griffin Quarles <gquarles@protonmail.com>
2020-04-16 13:03:41 -0400

    4

--- part2.cpp
+++ part2.cpp
@@ -8,6 +8,7 @@
 int main() {
 
 	int a[20] = { 100, 99, 97, 95, 90, 87, 86, 83, 81, 77, 74, 69, 63, 50, 44, 43, 39, 31, 29, 12 };
+	//int a[9] = { 123456, 342100, 87539, 606006, 443322, 198371, 99109, 88018, 707007 };
 	int n = 20;
 	int temp;
 	int location;
@@ -15,31 +16,37 @@ int main() {
 	for (int i = 1; i < n; i++) {
 		__asm {
 			mov ecx, i //Load iterator into ecx
-			mov eax, a[ecx*4] //Load a[i]
+			mov eax, a[ecx * 4] //Load a[i]
 			mov temp, eax       //temp = a[i]
 			dec ecx //dec location
 			mov location, ecx   //location = i - 1
 			inc ecx //Inc ecx after saving location
-		}
 
-		while (location >= 0 && a[location] > temp) {
-			__asm {
-				mov eax, location //Load in location
-				mov ebx, a[eax*4] //Load in the var from array into ebx
-				inc eax //incrment location
-				mov a[eax*4], ebx //Save var in array  //a[location + 1] = a[location];
-				dec eax //Decrement by 2               //location--;
-				dec eax //eax is location
-				mov location, eax //Save location
-			}
-			
-		}
 
-		__asm {
+			whiletop: nop //Top of while loop
+			cmp location, 0 //location >= 0
+			jl whileout //Leave while loop
+
+			mov eax, location //Load location into eax
+			mov ebx, a[eax*4] //Load a[location] into ebx
+			cmp ebx, temp //a[location > temp]
+			jl whileout //exit loop
+
+			//mov eax, location //Load in location
+			mov ebx, a[eax * 4] //Load in the var from array into ebx
+			inc eax //incrment location
+			mov a[eax * 4], ebx //Save var in array  //a[location + 1] = a[location];
+			dec eax //Decrement by 2               //location--;
+			dec eax //eax is location
+			mov location, eax //Save location
+			jmp whiletop //Go back to while loop (location >= 0 && a[location] > temp)
+
+			whileout:nop
 			mov eax, location //Move location into eax
 			inc eax //increment eax to go further in the array
 			mov ebx, temp //load temp into ebx
-			mov a[eax*4], ebx //save a[location + 1] = temp;
+			mov a[eax * 4], ebx //save a[location + 1] = temp;
+
 		}
 	}
 
